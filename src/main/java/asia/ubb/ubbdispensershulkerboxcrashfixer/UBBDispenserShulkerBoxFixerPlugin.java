e60951be2d6c71895d33ba7fdcc9dd81026cb2c3
Leung Yau Ming <leungyauming2005@gmail.com>
2021-02-16 12:11:25 +0800

    feat: add direction check to prevent false cancellation

--- src/main/java/asia/ubb/ubbdispensershulkerboxcrashfixer/UBBDispenserShulkerBoxFixerPlugin.java
+++ src/main/java/asia/ubb/ubbdispensershulkerboxcrashfixer/UBBDispenserShulkerBoxFixerPlugin.java
@@ -1,8 +1,11 @@
 package asia.ubb.ubbdispensershulkerboxcrashfixer;
 
+import org.bukkit.Material;
+import org.bukkit.block.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.material.Directional;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UBBDispenserShulkerBoxFixerPlugin extends JavaPlugin implements Listener {
@@ -14,10 +17,18 @@ public class UBBDispenserShulkerBoxFixerPlugin extends JavaPlugin implements Lis
 
     @EventHandler
     public void onBlockDispense(BlockDispenseEvent e) {
-        if (e.getBlock().getY() == 0 || e.getBlock().getY() == e.getBlock().getWorld().getMaxHeight()-1) {
-            if (e.getItem().getType().name().endsWith("SHULKER_BOX")) {
-                e.setCancelled(true);
-            }
+        Block b = e.getBlock();
+        if (b.getType() == Material.DISPENSER) {
+            BlockFace f = ((Directional) b.getState().getData()).getFacing();
+
+            boolean y0FacingDown = b.getY() == 0 &&
+                    f == BlockFace.DOWN;
+            boolean yMaxFacingDown = b.getY() == b.getWorld().getMaxHeight() - 1 &&
+                    f == BlockFace.UP;
+
+            if (y0FacingDown || yMaxFacingDown)
+                if (e.getItem().getType().name().toLowerCase().endsWith("shulker_box"))
+                    e.setCancelled(true);
         }
     }
 
