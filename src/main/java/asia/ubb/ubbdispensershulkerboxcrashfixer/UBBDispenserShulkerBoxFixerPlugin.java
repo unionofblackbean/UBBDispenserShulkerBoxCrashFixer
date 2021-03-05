f93239a9ac6e6faef6edee99051f4bc3c3235fcc
Leung Yau Ming <leungyauming2005@gmail.com>
2021-03-05 14:59:06 +0800

    refactor!: change logging strategy
    remove warning for player
    prefix language config file with msg_
    separate event handler and onEnable
    use new config options
    use new language config options
    
    BREAKING CHANGE

--- src/main/java/asia/ubb/ubbdispensershulkerboxcrashfixer/UBBDispenserShulkerBoxFixerPlugin.java
+++ src/main/java/asia/ubb/ubbdispensershulkerboxcrashfixer/UBBDispenserShulkerBoxFixerPlugin.java
@@ -1,11 +1,10 @@
 package asia.ubb.ubbdispensershulkerboxcrashfixer;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
@@ -16,72 +15,59 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.List;
 
-public class UBBDispenserShulkerBoxFixerPlugin extends JavaPlugin {
+public class UBBDispenserShulkerBoxFixerPlugin extends JavaPlugin implements Listener {
 
-    private static String playerMessage;
-    private static String consoleMessage;
+    private final Configuration config = this.getConfig();
+    private final boolean consoleLogMessages = this.config.getBoolean("log.console");
+    private final String defaultConsoleMessage = "A dispenser at ({0}, {1}, {2}) in world {3} tries to dispense a shulker box with items out of the would. Dispensation was cancelled.";
+    private String consoleMessage = null;
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
 
-        final String localeKey = getConfig().getString("Language");
-        File langFile = new File(this.getDataFolder(), localeKey + ".yml");
+        String langCode = this.config.getString("lang").trim();
+        String langFileName = "msg_" + langCode + ".yml";
+
+        File langFile = new File(this.getDataFolder(), langFileName);
         if (!langFile.exists())
-            this.saveResource(localeKey + ".yml", false);
+            this.saveResource(langFileName, false);
 
-        try {
-            FileConfiguration lang = YamlConfiguration.loadConfiguration(langFile);
-            playerMessage = ChatColor.translateAlternateColorCodes('&', lang.getString("PlayerMessage"));
-            consoleMessage = ChatColor.translateAlternateColorCodes('&', lang.getString("ConsoleMessage"));
-        } catch (Throwable e) {
-            getLogger().severe(
-                    "Failed to load locale " + localeKey + " because file doesn't exist. We will use English as default message language.");
-            playerMessage =
-                    "A dispenser at the top or bottom of the world and near you wants to dispense a shulker box with items. We have blocked it.";
-            consoleMessage =
-                    "A dispenser({0}, {1}, {2}, {3}) at the top or bottom of the world wants to dispense a shulker box with items. The players near it are {4}. We have blocked it.";
-        }
+        FileConfiguration langMessagesConfig = YamlConfiguration.loadConfiguration(langFile);
+        this.consoleMessage = ChatColor.translateAlternateColorCodes('&',
+                langMessagesConfig.getString(
+                        "message.console",
+                        this.defaultConsoleMessage));
 
-        this.getServer().getPluginManager().registerEvents(new Listener() {
-            @EventHandler
-            public void onBlockDispense(BlockDispenseEvent e) {
-                final Block b = e.getBlock();
-                if (b.getType() == Material.DISPENSER) {
-                    final BlockFace f = ((Directional) b.getState().getData()).getFacing();
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
 
-                    final boolean y0FacingDown = b.getY() == 0 &&
-                            f == BlockFace.DOWN;
-                    final boolean yMaxFacingDown = b.getY() == b.getWorld().getMaxHeight() - 1 &&
-                            f == BlockFace.UP;
+    @EventHandler
+    public void onBlockDispense(BlockDispenseEvent e) {
+        Block b = e.getBlock();
+        if (b.getType() == Material.DISPENSER) {
+            BlockFace f = ((Directional) b.getState().getData()).getFacing();
 
-                    if (y0FacingDown || yMaxFacingDown) {
-                        if (e.getItem().getType().name().toLowerCase().endsWith("shulker_box")) {
-                            e.setCancelled(true);
+            boolean y0FacingDown = b.getY() == 0 &&
+                    f == BlockFace.DOWN;
+            boolean yMaxFacingDown = b.getY() == b.getWorld().getMaxHeight() - 1 &&
+                    f == BlockFace.UP;
 
-                            final Location loc = b.getLocation();
-                            final List<String> players = new ArrayList<>();
-                            Bukkit.getOnlinePlayers().forEach(player -> {
-                                final Location playerLoc = player.getLocation();
-                                if (playerLoc.distance(loc) <= 10.0) {
-                                    player.sendMessage(playerMessage);
-                                    players.add(player.getName());
-                                }
-                            });
+            if (y0FacingDown || yMaxFacingDown) {
+                if (e.getItem().getType().name().toLowerCase().endsWith("shulker_box")) {
+                    e.setCancelled(true);
 
-                            getLogger().info(MessageFormat.format(consoleMessage,
-                                    loc.getWorld().getName(),
-                                    loc.getBlockX(),
-                                    loc.getBlockY(),
-                                    loc.getBlockZ(),
-                                    players));
-                        }
-                    }
+                    if (consoleLogMessages)
+                        this.getLogger().info(
+                                MessageFormat.format(
+                                        consoleMessage == null ? this.defaultConsoleMessage : this.consoleMessage,
+                                        b.getX(),
+                                        b.getY(),
+                                        b.getZ(),
+                                        b.getWorld().getName()));
                 }
             }
-        }, this);
+        }
     }
 }
